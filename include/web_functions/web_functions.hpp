b2ead48f08ca1608549854ff4c76a8e25861df35
Dario Cammi <dario.cammi@agenoria.it>
2025-08-22 15:20:52 +0200

    Added X motor group in WebFunctions

--- include/web_functions/web_functions.hpp
+++ include/web_functions/web_functions.hpp
@@ -12,20 +12,22 @@ class WebFunctions {
 
         IOBoard& _ioBoard;
         EncoderMultiJog& _encoderJog;
+        IMotorHoming& _XMotor;
         IMotorHoming& _YMotor;
         IMotorHoming& _LMotor;
         IMotorHoming& _RMotor;
         WebFunctionGroupAxes _axesGroup = WebFunctionGroupAxes(_ioBoard, _taskRunner, _encoderJog);
+        WebFunctionGroupAxis _xAxisGroup = WebFunctionGroupAxis("x_axis", "X-Axis", _ioBoard, _taskRunner, _XMotor);
         WebFunctionGroupAxis _yAxisGroup = WebFunctionGroupAxis("y_axis", "Y-Axis", _ioBoard, _taskRunner, _YMotor);
         WebFunctionGroupAxis _lAxisGroup = WebFunctionGroupAxis("l_axis", "L-Axis", _ioBoard, _taskRunner, _LMotor);
         WebFunctionGroupAxis _rAxisGroup = WebFunctionGroupAxis("r_axis", "R-Axis", _ioBoard, _taskRunner, _RMotor);
 
-        WebFunctionGroup* _groups[4] = { &_axesGroup, &_yAxisGroup, &_lAxisGroup, &_rAxisGroup };
+        WebFunctionGroup* _groups[5] = { &_axesGroup, &_xAxisGroup, &_yAxisGroup, &_lAxisGroup, &_rAxisGroup };
         uint16_t _groupsCount = sizeof(_groups) / sizeof(WebFunctionGroup*);
 
     public:
-        WebFunctions(IOBoard& ioBoard, EncoderMultiJog& encoderJog, IMotorHoming& YMotor, IMotorHoming& LMotor, IMotorHoming& RMotor)
-            : _ioBoard(ioBoard), _encoderJog(encoderJog), _YMotor(YMotor), _LMotor(LMotor), _RMotor(RMotor) {}
+        WebFunctions(IOBoard& ioBoard, EncoderMultiJog& encoderJog, IMotorHoming& XMotor, IMotorHoming& YMotor, IMotorHoming& LMotor, IMotorHoming& RMotor)
+            : _ioBoard(ioBoard), _encoderJog(encoderJog), _XMotor(XMotor), _YMotor(YMotor), _LMotor(LMotor), _RMotor(RMotor) {}
 
         WebFunctionGroup* getGroup(const char* name);
 
